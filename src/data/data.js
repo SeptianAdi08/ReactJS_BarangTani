54a5bdc0a1489f3c7dca386b123bd1e09dd0c411
SeptianAdi08 <nugrohoseptian48@gmail.com>
2024-01-11 15:56:57 +0700

    modifikasi data.js

---
+++ src/data/data.js
@@ -0,0 +1,90 @@
+export const data = [
+    {
+        id: 1,
+        name: 'Double Cheesburger',
+        category: 'burger',
+        image:
+        'https://images.unsplash.com/photo-1611270629569-8b357cb88da9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGFzdGF8ZW58MHx8MHx8fDA%3D',
+        price: '$$$$$',
+    },
+    {
+        id: 2,
+        name: 'Double Cheesburger',
+        category: 'burger',
+        image:
+        'https://images.unsplash.com/photo-1595295333158-4742f28fbd85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBhc3RhfGVufDB8fDB8fHww',
+        price: '$$$$$',
+    },
+    {
+        id: 3,
+        name: 'Double Cheesburger',
+        category: 'burger',
+        image:
+        'https://images.unsplash.com/photo-1595295333158-4742f28fbd85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBhc3RhfGVufDB8fDB8fHww',
+        price: '$$$$$',
+    },
+    {
+        id: 4,
+        name: 'Double Cheesburger',
+        category: 'burger',
+        image:
+        'https://images.unsplash.com/photo-1595295333158-4742f28fbd85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBhc3RhfGVufDB8fDB8fHww',
+        price: '$$$$$',
+    },
+    {
+        id: 5,
+        name: 'Double Cheesburger',
+        category: 'burger',
+        image:
+        'https://images.unsplash.com/photo-1595295333158-4742f28fbd85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBhc3RhfGVufDB8fDB8fHww',
+        price: '$$$$$',
+    },
+    {
+        id: 6,
+        name: 'Double Cheesburger',
+        category: 'burger',
+        image:
+        'https://images.unsplash.com/photo-1595295333158-4742f28fbd85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBhc3RhfGVufDB8fDB8fHww',
+        price: '$$$$$',
+    },
+
+
+];
+
+export const categories = [
+    {
+        id: 1,
+        name: 'Fast Food',
+        image: 
+        'https://images.unsplash.com/photo-1595295333158-4742f28fbd85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBhc3RhfGVufDB8fDB8fHww',
+
+    },
+    {
+        id: 2,
+        name: 'Fast Food',
+        image: 
+        'https://images.unsplash.com/photo-1595295333158-4742f28fbd85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBhc3RhfGVufDB8fDB8fHww',
+
+    },
+    {
+        id: 3,
+        name: 'Fast Food',
+        image: 
+        'https://images.unsplash.com/photo-1595295333158-4742f28fbd85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBhc3RhfGVufDB8fDB8fHww',
+
+    },
+    {
+        id: 4,
+        name: 'Fast Food',
+        image: 
+        'https://images.unsplash.com/photo-1595295333158-4742f28fbd85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBhc3RhfGVufDB8fDB8fHww',
+
+    },
+    {
+        id: 5,
+        name: 'Fast Food',
+        image: 
+        'https://images.unsplash.com/photo-1595295333158-4742f28fbd85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBhc3RhfGVufDB8fDB8fHww',
+
+    },
+]
