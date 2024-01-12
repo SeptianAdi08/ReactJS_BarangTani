293e5f40af4f604538d9ccfa191cee861b0a4374
SeptianAdi08 <nugrohoseptian48@gmail.com>
2024-01-12 23:54:36 +0700

    add food filter in Food.jsx

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,10 +1,37 @@
-import React from "react";
-
+import React from "react"
+import {data} from '../data/data.js'
 
 const Food = () => {
+    console.log(data)
     return (
         
-        <div>Food</div>
+        <div>
+            <h1>Top Rate Menu Items </h1>
+
+            {/* Filter Row */}
+            <div>
+                 {/* Filter Type */}
+                <div>
+                    <p>Filter Type</p>
+                    <div>
+                        <button>All</button>
+                        <button>Chicken</button>
+                        <button>Salad</button>
+                    </div>
+                </div>
+
+                {/** Filter Price */}
+                <div>
+                    <p>Filter Price</p>
+                    <div>
+                        <button>$$</button>
+                         <button>$$$</button>
+                          <button>$</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
-}
+};
+
  export default Food
