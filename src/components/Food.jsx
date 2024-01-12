61d5875cdb9bac1f10675e6ff9ab6fa01e67604d
SeptianAdi08 <nugrohoseptian48@gmail.com>
2024-01-13 00:41:32 +0700

    style ini Food.jsx

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -5,28 +5,30 @@ const Food = () => {
     console.log(data)
     return (
         
-        <div>
-            <h1>Top Rate Menu Items </h1>
+        <div className=" max-w-[1640px] m-auto px-4 py-12">
+            <h1 className=" text-orange-600 font-bold text-4xl text-center">Top Rate Menu Items </h1>
 
             {/* Filter Row */}
-            <div>
+            <div className=" flex flex-col lg:flex-row justify-between">
                  {/* Filter Type */}
                 <div>
-                    <p>Filter Type</p>
-                    <div>
-                        <button>All</button>
-                        <button>Chicken</button>
-                        <button>Salad</button>
+                    <p className=" font-bold text-gray-700">Filter Type</p>
+                    <div className=" flex justify-between flex-wrap">
+                        <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">All</button>
+                        <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Chicken</button>
+                        <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Salad</button>
+                        <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">pizza</button>
                     </div>
                 </div>
 
                 {/** Filter Price */}
                 <div>
-                    <p>Filter Price</p>
-                    <div>
-                        <button>$$</button>
-                         <button>$$$</button>
-                          <button>$</button>
+                    <p className=" font-bold text-gray-700">Filter Price</p>
+                    <div className=" flex justify-between max-w-[390px] w-full">
+                        <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$$</button>
+                        <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$$$</button>
+                         <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$$$</button>
+                          <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$</button>
                     </div>
                 </div>
             </div>
