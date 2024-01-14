6736da625267d5903731ae90a69bbd57fee1f095
SeptianAdi08 <nugrohoseptian48@gmail.com>
2024-01-14 18:25:22 +0700

    Add disply foods

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import {data} from '../data/data.js'
 
 const Food = () => {
-    console.log(data)
+ //   console.log(data)
+ const [foods, setFoods] = useState(data)
     return (
         
         <div className=" max-w-[1640px] m-auto px-4 py-12">
@@ -31,6 +32,23 @@ const Food = () => {
                           <button className=" m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$</button>
                     </div>
                 </div>
+            </div>
+{/*Display foods*/}
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
+                {foods.map((item, index)=>(
+                    <div  key={index} className=" border shadow-lg rounded-lg hover:scale-105 duration-300">
+                        <img src={item.image} alt={item.name}
+                        className=" w-full h-[200px] object-cover rounded-t-lg" />
+                        <div className=" flex justify-between px-2 py-4">
+                            <p className=" font-bold">{item.name}</p>
+                            <p>
+                                <span className=" bg-orange-500 text-white p-1 rounded-full">{item.price}</span>
+                            </p>
+                            </div>
+
+                    </div>
+                ))}
+
             </div>
         </div>
     )
